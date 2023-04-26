118e4191c16b92a1942b811c49be70dfad0fa6e3
Vishwa Protim <vishwaprotimbanerjee93@gmail.com>
2023-04-26 11:10:58 +0530

    Created Batch Skeleton

---
+++ flatfile/src/main/java/com/protim/batch/flatfile/FlatfileApplication.java
@@ -0,0 +1,13 @@
+package com.protim.batch.flatfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatfileApplication.class, args);
+	}
+
+}
---
+++ flatfile/src/test/java/com/protim/batch/flatfile/FlatfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protim.batch.flatfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
