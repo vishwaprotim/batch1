1b9e541718430e701bd969cbee7ec841fbc92dd3
Vishwa Protim <vishwaprotimbanerjee93@gmail.com>
2023-05-02 18:16:43 +0530

    Created Reader, initial processor creation

---
+++ flatfile/src/main/java/com/protim/batch/config/RecordProcessor.java
@@ -0,0 +1,9 @@
+package com.protim.batch.config;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.protim.batch.entity.Record;
+
+public class RecordProcessor implements ItemProcessor<Record, Record> {
+
+}
--- flatfile/src/main/java/com/protim/batch/config/SpringBatchConfig.java
+++ flatfile/src/main/java/com/protim/batch/config/SpringBatchConfig.java
@@ -1,5 +1,51 @@
 package com.protim.batch.config;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import com.protim.batch.entity.Record;
+
+@Configuration
+@EnableBatchProcessing
 public class SpringBatchConfig {
 
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public FlatFileItemReader<Record> reader() {
+        FlatFileItemReader<Record> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/input.csv"));
+        itemReader.setName("csvFileReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+
+    }
+
+    private LineMapper<Record> lineMapper() {
+        DefaultLineMapper<Record> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer(",");
+        delimitedLineTokenizer.setStrict(false);
+        delimitedLineTokenizer.setNames("id", "name", "subject", "marks", "spec");
+
+        BeanWrapperFieldSetMapper<Record> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Record.class);
+
+        lineMapper.setLineTokenizer(delimitedLineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper();
+    }
 }
