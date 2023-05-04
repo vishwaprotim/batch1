2e43d857f4b480a8af3ad30f31eb0e3d4c77c5ab
Vishwa Protim <vishwaprotimbanerjee93@gmail.com>
2023-05-04 11:21:57 +0530

    Refining writer logic with logger anď anonymous class

--- flatfile/src/main/java/com/protim/batch/config/SpringBatchConfig.java
+++ flatfile/src/main/java/com/protim/batch/config/SpringBatchConfig.java
@@ -1,7 +1,11 @@
 package com.protim.batch.config;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
@@ -10,14 +14,11 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.FieldExtractor;
-import org.springframework.batch.item.file.transform.LineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,10 +30,8 @@ import com.protim.batch.entity.Record;
 @EnableBatchProcessing
 public class SpringBatchConfig extends DefaultBatchConfigurer {
 
-    @Override
-    public void setDataSource(DataSource dataSource) {
-        // This BatchConfigurer ignores any DataSource
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBatchConfig.class);
+    private static final int CHUNK = 100;
 
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
@@ -40,16 +39,34 @@ public class SpringBatchConfig extends DefaultBatchConfigurer {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
+    @Override
+    public void setDataSource(DataSource dataSource) {
+        // This BatchConfigurer ignores any DataSource
+    }
+
     @Bean
     public FlatFileItemReader<Record> reader() {
         FlatFileItemReader<Record> itemReader = new FlatFileItemReader<>();
         itemReader.setResource(new FileSystemResource("flatfile/src/main/resources/input.csv"));
         itemReader.setName("csvFileReader");
         itemReader.setLinesToSkip(1);
-        itemReader.setLineMapper(lineMapper());
-
+        itemReader.setLineMapper(new DefaultLineMapper<>() {
+            {
+                setLineTokenizer(new DelimitedLineTokenizer() {
+                    {
+                        setNames("id", "name", "subject", "marks", "spec");
+                        setStrict(false);
+                        setDelimiter(DELIMITER_COMMA);
+                    }
+                });
+                setFieldSetMapper(new BeanWrapperFieldSetMapper<Record>() {
+                    {
+                        setTargetType(Record.class);
+                    }
+                });
+            }
+        });
         return itemReader;
-
     }
 
     @Bean
@@ -59,16 +76,32 @@ public class SpringBatchConfig extends DefaultBatchConfigurer {
 
     @Bean
     public FlatFileItemWriter<Record> writer() {
-        FlatFileItemWriter<Record> writer = new FlatFileItemWriter<>();
+        FlatFileItemWriter<Record> writer = new FlatFileItemWriter<>() {
+            @Override
+            public String doWrite(List<? extends Record> items) {
+                LOGGER.info("csvWriter invoked for writing #lines: " + items.size());
+                return super.doWrite(items);
+            }
+        };
+        writer.setName("csvWriter");
         writer.setResource(new FileSystemResource("flatfile/output/output.csv"));
         writer.setAppendAllowed(true);
-        writer.setLineAggregator(aggregator());
+        writer.setLineAggregator(new DelimitedLineAggregator<>() {
+            {
+                setDelimiter(",");
+                setFieldExtractor(new BeanWrapperFieldExtractor<>() {
+                    {
+                        setNames(new String[] { "id", "name", "subject", "grade", "marks", "spec" });
+                    }
+                });
+            }
+        });
         return writer;
     }
 
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("process-csv").<Record, Record>chunk(10)
+        return stepBuilderFactory.get("process-csv").<Record, Record>chunk(CHUNK)
                 .reader(reader())
                 .processor(processor())
                 .writer(writer()).build();
@@ -80,30 +113,4 @@ public class SpringBatchConfig extends DefaultBatchConfigurer {
                 .flow(step1()).end().build();
     }
 
-    private LineAggregator<Record> aggregator() {
-        DelimitedLineAggregator<Record> aggregator = new DelimitedLineAggregator<>();
-        aggregator.setDelimiter(",");
-        aggregator.setFieldExtractor(fieldExtractor());
-        return aggregator;
-    }
-
-    private FieldExtractor<Record> fieldExtractor() {
-        BeanWrapperFieldExtractor<Record> extractor = new BeanWrapperFieldExtractor<>();
-        extractor.setNames(new String[] { "id", "name", "subject", "grade", "marks", "spec" });
-        return extractor;
-    }
-
-    private LineMapper<Record> lineMapper() {
-        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer(",");
-        delimitedLineTokenizer.setStrict(false);
-        delimitedLineTokenizer.setNames("id", "name", "subject", "marks", "spec");
-
-        BeanWrapperFieldSetMapper<Record> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
-        fieldSetMapper.setTargetType(Record.class);
-
-        DefaultLineMapper<Record> lineMapper = new DefaultLineMapper<>();
-        lineMapper.setLineTokenizer(delimitedLineTokenizer);
-        lineMapper.setFieldSetMapper(fieldSetMapper);
-        return lineMapper;
-    }
 }
---
+++ flatfile/src/main/java/com/protim/batch/util/Helper.java
@@ -0,0 +1,43 @@
+package com.protim.batch.util;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Utility Class for CSV Batch
+ */
+public class Helper {
+
+    private static String[] names = { "Amit", "John", "David", "Mary", "Carlos", "Subir", "Chris" };
+    private static String[] subjects = { "Physics", "Biology", "Mathematics", "History", "Arts" };
+    private static int[] marks = { 100, 98, 65, 44 };
+    private static String[] specs = { "major", "minor" };
+
+    private static final String FILE = "input.csv";
+
+    /**
+     * Creates CSV File for the batch to process
+     */
+    public static void createCsv() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE));) {
+            String header = "\"Id\",\"Name\",\"Subject\",\"Marks\",\"Specs\"\n";
+            writer.write(header);
+            for (int i = 0; i < 50000; i++) {
+                int name = (int) (Math.random() * (6 - 0 + 1) + 0);
+                int subject = (int) (Math.random() * (4 - 0 + 1) + 0);
+                int mark = (int) (Math.random() * (3 - 0 + 1) + 0);
+                int spec = (int) (Math.random() * (1 - 0 + 1) + 0);
+                String line = (i + 1) + "," + "\"" + names[name] + "\"" + "," + "\"" + subjects[subject] + "\"" + ","
+                        + marks[mark]
+                        + "," + "\"" + specs[spec] + "\"";
+                System.out.println("Line[" + i + "]: " + line);
+                writer.write(line + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
